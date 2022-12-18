4c4f54b6f7b50a54b51a730278225bb1ba990dd0
Alexandre Piva <alexandrehpiva@gmail.com>
2022-12-18 07:20:15 -0300

    Add dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,7 +5,6 @@ const Dashboard: React.FC = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      {/* <button onClick={() => window.history.back()}>Voltar</button> */}
       <Link to="/">Voltar</Link>
     </div>
   )
